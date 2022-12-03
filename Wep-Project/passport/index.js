1da802244d6ae500a0bf80911cd04719b3945252
Daniel <clwnssla1@bible.ac.kr>
2022-12-03 22:42:18 +0900

    feat: Wep-Project 파일 생성(step4 copy)

---
+++ Wep-Project/passport/index.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+const local = require('./local');
+const kakao = require('./kakao');
+const User = require('../models/user');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => { // 로그인 요청 처리를 위한 미들웨어
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => { // 로그인 처리를 해주는 미들웨어
+    User.findOne({
+      where: { id }
+    })
+    .then(user => done(null, user))
+    .catch(err => done(err));
+  });
+
+  local();
+  kakao();
+};
